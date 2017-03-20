01728d219c466e4632101893029a626b120777a0
skaveesh <skaveesh@gmail.com>
2017-03-20 15:14:23 +0530

    Initial commit

---
+++ src/main/java/com/expenx/ExpenXApplication.java
@@ -0,0 +1,12 @@
+package com.expenx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenXApplication.class, args);
+	}
+}
